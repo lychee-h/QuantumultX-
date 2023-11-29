756890f27d1c4e2588ecc86dacb8b12a90b8e9fc
lychee-h <v2293136310@163.com>
2023-11-29 15:07:24 +0800

    zaobao

--- scripts/zaobao.js
+++ scripts/zaobao.js
@@ -1,6 +1,7 @@
 /*************************************
 项目名称：联合早报
 脚本作者：Henry
+更新日期：2023/11/29
 **************************************
 
 [rewrite_local]
@@ -8,7 +9,7 @@
 ^https:\/\/nsdd\.zaobao\.com\/appapi\/feed\/home\/zb\?[^?]*$ url script-response-body https://raw.githubusercontent.com/lychee-h/QuantumultX-backup/main/scripts/zaobao.js
 
 [mitm] 
-hostname = zaobao.com
+hostname = *.zaobao.com
 
 *************************************/
 
